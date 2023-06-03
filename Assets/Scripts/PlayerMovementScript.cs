b8a3eb75fa931f14e5f0acf6fc727b628735bbfc
ピクセル GM <35326620+PixelGM@users.noreply.github.com>
2023-06-02 18:08:28 -0700

    No Delay in Movement

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -10,6 +10,9 @@ public class PlayerMovementScript : MonoBehaviour
     public float testKnockbackForce = 10;
     private Rigidbody2D _body;
     private Camera _cam;
+    private bool _jumpRequest;
+    private bool _knockbackRequest;
+    private Vector2 _knockbackDirection;
 
     private void Awake()
     {
@@ -19,23 +22,40 @@ public class PlayerMovementScript : MonoBehaviour
 
     private void Update()
     {
-        Vector2 force = Vector2.zero;
+        float xInput = 0;
         
-        if (Input.GetKey(KeyCode.A)) force += Vector2.left;
-        if (Input.GetKey(KeyCode.D)) force += Vector2.right;
-        
-        _body.AddForce(force * (Time.deltaTime * speed), ForceMode2D.Force);
-        
-        if (Input.GetKeyDown(KeyCode.Space)) _body.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
+        if (Input.GetKey(KeyCode.A)) xInput -= 1;
+        if (Input.GetKey(KeyCode.D)) xInput += 1;
 
+        // Apply the speed directly to the x velocity, preserving the y velocity
+        _body.velocity = new Vector2(xInput * speed, _body.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space)) _jumpRequest = true;
+        
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 worldMousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
             Vector2 dirVec = ((Vector2)(transform.position - worldMousePos)).normalized;
             if (dirVec != Vector2.zero)
             {
-                _body.AddForce(dirVec * testKnockbackForce, ForceMode2D.Impulse);
+                _knockbackRequest = true;
+                _knockbackDirection = dirVec;
             }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_jumpRequest)
+        {
+            _body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            _jumpRequest = false;
+        }
+
+        if (_knockbackRequest)
+        {
+            _body.AddForce(_knockbackDirection * testKnockbackForce, ForceMode2D.Impulse);
+            _knockbackRequest = false;
         }
     }
 }
