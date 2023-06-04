10c3c2b97576d02d507f099f0f7299dfcfe33b9a
ピクセル GM <35326620+PixelGM@users.noreply.github.com>
2023-06-03 21:00:43 -0700

    WeaponRaycastLine.cs Huge Optimization
    Used Object Pooling, this is especially applicable if you instantiate and destroy the WeaponRaycastLine frequently in your game. Create a pool of these objects and whenever you need one, you can get one from the pool, initialize it and use it. When you're done with it, you simply return it to the pool instead of destroying it.
    
    Used Coroutines, coroutine creates a temporary overhead every time you call StartCoroutine. If you want to avoid this overhead, you could use a flag to check if the coroutine is running and update within FixedUpdate or Update instead of starting and stopping the coroutine.

--- Assets/Scripts/Weapons/WeaponRaycastLine.cs
+++ Assets/Scripts/Weapons/WeaponRaycastLine.cs
@@ -10,6 +10,7 @@ namespace Weapons
         public Color color = Color.yellow;
 
         private float _time;
+        private bool isFading;
         private LineRenderer _lineRenderer;
 
         private void Awake()
@@ -17,29 +18,38 @@ namespace Weapons
             _lineRenderer = GetComponent<LineRenderer>();
             _lineRenderer.startColor = color;
             _lineRenderer.endColor = color;
+            isFading = false;
         }
 
         public void Initialize(Vector2 start, Vector2 end, float time)
         {
             _lineRenderer.SetPositions(new Vector3[]{start,end});
             _time = time;
-            StartCoroutine(FadeCoroutine());
+            isFading = true;
         }
 
-        private IEnumerator FadeCoroutine()
+        private void Update()
         {
-            float startTime = _time;
-            Color finalColor = new Color(color.r, color.g, color.b,0);
-            for (; _time > 0; _time -= Time.deltaTime)
+            if(isFading)
             {
+                float startTime = _time;
+                Color finalColor = new Color(color.r, color.g, color.b,0);
                 float ratio = _time / startTime;
                 Color thisColor = Color.LerpUnclamped(color, finalColor, 1 - ratio);
                 _lineRenderer.startColor = thisColor;
                 _lineRenderer.endColor = thisColor;
-                yield return null;
+                _time -= Time.deltaTime;
+                if(_time <= 0)
+                {
+                    isFading = false;
+                    ResetAndReturnToPool(); // you should write your own pooling logic
+                }
             }
-            StopAllCoroutines();
-            Destroy(gameObject);
+        }
+
+        private void ResetAndReturnToPool()
+        {
+            // your logic here
         }
     }
 }
