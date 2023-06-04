53d46151b10735e019409a0cf8231ece865b363e
ピクセル GM <35326620+PixelGM@users.noreply.github.com>
2023-06-03 21:03:42 -0700

    Revert "WeaponRaycastLine.cs Huge Optimization"
    This reverts commit 10c3c2b97576d02d507f099f0f7299dfcfe33b9a.

--- Assets/Scripts/Weapons/WeaponRaycastLine.cs
+++ Assets/Scripts/Weapons/WeaponRaycastLine.cs
@@ -10,7 +10,6 @@ namespace Weapons
         public Color color = Color.yellow;
 
         private float _time;
-        private bool isFading;
         private LineRenderer _lineRenderer;
 
         private void Awake()
@@ -18,38 +17,29 @@ namespace Weapons
             _lineRenderer = GetComponent<LineRenderer>();
             _lineRenderer.startColor = color;
             _lineRenderer.endColor = color;
-            isFading = false;
         }
 
         public void Initialize(Vector2 start, Vector2 end, float time)
         {
             _lineRenderer.SetPositions(new Vector3[]{start,end});
             _time = time;
-            isFading = true;
+            StartCoroutine(FadeCoroutine());
         }
 
-        private void Update()
+        private IEnumerator FadeCoroutine()
         {
-            if(isFading)
+            float startTime = _time;
+            Color finalColor = new Color(color.r, color.g, color.b,0);
+            for (; _time > 0; _time -= Time.deltaTime)
             {
-                float startTime = _time;
-                Color finalColor = new Color(color.r, color.g, color.b,0);
                 float ratio = _time / startTime;
                 Color thisColor = Color.LerpUnclamped(color, finalColor, 1 - ratio);
                 _lineRenderer.startColor = thisColor;
                 _lineRenderer.endColor = thisColor;
-                _time -= Time.deltaTime;
-                if(_time <= 0)
-                {
-                    isFading = false;
-                    ResetAndReturnToPool(); // you should write your own pooling logic
-                }
+                yield return null;
             }
-        }
-
-        private void ResetAndReturnToPool()
-        {
-            // your logic here
+            StopAllCoroutines();
+            Destroy(gameObject);
         }
     }
 }
