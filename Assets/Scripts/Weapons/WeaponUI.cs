2579cb0edb962cfc66b69bfec579bca153cb7278
Z0rb14n <48940672+Z0rb14n@users.noreply.github.com>
2023-06-03 18:26:14 -0700

    Preliminary Weapon UI; Projectiles; Reload + Clip; Burst; Linecast Melee; TMP Import
    * Fire Projectiles
    
    * Reload, clip
    
    * TextMeshPro
    
    * Funny UI
    
    * Burst Rifle
    
    * Hacky Melee
    
    * Move canvas to prefab
    
    * SILENCE

---
+++ Assets/Scripts/Weapons/WeaponUI.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Weapons
+{
+    public class WeaponUI : MonoBehaviour
+    {
+        public Weapon weapon;
+        public TextMeshProUGUI textObject;
+
+        public void Update()
+        {
+            textObject.text = $"{weapon.weaponData.weaponName}: {weapon.weaponData.Clip}/{weapon.weaponData.clipSize}" + (weapon.ReloadTime <= 0
+                ? ""
+                : $" Reload Time: {weapon.ReloadTime:F2}");
+        }
+    }
+}
