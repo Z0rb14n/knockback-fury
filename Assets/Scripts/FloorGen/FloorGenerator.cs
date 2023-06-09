a128c809092e3ed07db3d2a8673b5621b94ad06f
Z0rb14n <babybluewoof@gmail.com>
2023-06-08 20:30:57 -0700

    um

--- Assets/Scripts/FloorGen/FloorGenerator.cs
+++ Assets/Scripts/FloorGen/FloorGenerator.cs
@@ -1,16 +1,86 @@
+using System;
 using UnityEngine;
+
+using Cell = System.ValueTuple<int, int>;
+using Grid = System.Collections.Generic.Dictionary<System.ValueTuple<int, int>, FloorGen.RoomType>;
+using Random = System.Random;
 
 namespace FloorGen
 {
     public class FloorGenerator : MonoBehaviour
     {
         public Pair[] pairs;
+        public int seed = 0;
+        public int pathLength = 4;
+        public int pathHeight = 1;
+
+        private void Awake()
+        {
+            Generate();
+        }
+
+        public void Generate()
+        {
+            Grid grid = new();
+            Random random;
+            if (seed == 0)
+            {
+                int guidHash = Guid.NewGuid().GetHashCode();
+                random = new Random(guidHash);
+                Debug.Log(guidHash.ToString());
+            }
+            else
+                random = new Random(seed);
+
+            int remainingLength = pathLength;
+            int remainingHeight = pathHeight;
+            Cell currCell = (0, 0);
+            while (remainingLength > 0)
+            {
+                int upwardBias = remainingHeight - (remainingLength >> 1);
+                bool shouldGoUp = CalculateRandom(random, upwardBias);
+                bool shouldGoDown = !shouldGoUp && CalculateRandom(random, -upwardBias);
+                ExpandSide(grid, currCell, RoomType.RightOpen);
+                currCell = RoomType.RightOpen.Move(currCell);
+                if (shouldGoUp)
+                {
+                    ExpandSide(grid, currCell, RoomType.TopOpen);
+                    currCell = RoomType.TopOpen.Move(currCell);
+                } else if (shouldGoDown)
+                {
+                    ExpandSide(grid, currCell, RoomType.BottomOpen);
+                    currCell = RoomType.BottomOpen.Move(currCell);
+                }
+
+                remainingLength--;
+            }
+
+        }
+
+        private static void ExpandSide(Grid grid, Cell cell, RoomType dir)
+        {
+            // can't be mixed
+            Debug.Assert(dir is RoomType.BottomOpen or RoomType.LeftOpen or RoomType.RightOpen or RoomType.TopOpen);
+            cell = dir.Move(cell);
+            if (grid.ContainsKey(cell))
+                grid[cell] |= dir.GetOpposing();
+            else
+                grid[cell] = dir.GetOpposing();
+
+        }
+
+        private static bool CalculateRandom(Random random, int bias)
+        {
+            float exp = Mathf.Exp(bias);
+            float prob = exp / (1 + exp);
+            return random.Next(0, 100) > prob * 100;
+        }
     }
 
-    [System.Serializable]
+    [Serializable]
     public struct Pair
     {
         public RoomType type;
-        public GameObject[] go;
+        public GameObject[] roomPrefab;
     }
 }
--- Assets/Scripts/FloorGen/RoomType.cs
+++ Assets/Scripts/FloorGen/RoomType.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+using Cell = System.ValueTuple<int, int>;
+using Grid = System.Collections.Generic.Dictionary<System.ValueTuple<int, int>, FloorGen.RoomType>;
+
 namespace FloorGen
 {
     [Flags]
@@ -9,5 +12,32 @@ namespace FloorGen
         TopOpen = 2,
         RightOpen = 4,
         BottomOpen = 8
+    }
+
+    public static class RoomTypeUtil
+    {
+        public static RoomType GetOpposing(this RoomType type)
+        {
+            return type switch
+            {
+                RoomType.LeftOpen => RoomType.RightOpen,
+                RoomType.BottomOpen => RoomType.TopOpen,
+                RoomType.RightOpen => RoomType.LeftOpen,
+                RoomType.TopOpen => RoomType.BottomOpen,
+                _ => 0
+            };
+        }
+
+        public static Cell Move(this RoomType type, Cell cell)
+        {
+            return type switch
+            {
+                RoomType.LeftOpen => (cell.Item1-1,cell.Item2),
+                RoomType.BottomOpen => (cell.Item1,cell.Item2-1),
+                RoomType.RightOpen => (cell.Item1+1,cell.Item2),
+                RoomType.TopOpen => (cell.Item1,cell.Item2+1),
+                _ => cell
+            };
+        }
     }
 }
