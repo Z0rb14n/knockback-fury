a55552c377b321fe156ff25a694050328740cd4a
J. L <liubusiness42@gmail.com>
2023-06-21 19:41:51 -0700

    Created ranged enemy and bullet, created script for shooting and noticing the player

---
+++ Assets/Scripts/Enemies/RangedEShootScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
