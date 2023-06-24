6aa96792ef820aa7458a999049df7c92cb13786b
Z0rb14n <48940672+Z0rb14n@users.noreply.github.com>
2023-06-23 19:52:32 -0700

    New weapons (#28)
    * Weapon Right Click Toggle; Assault Rifle + SemiAutoShotgun; Projectile Rotation
    
    * Grenade Launcher
    
    * Dual Revolvers

---
+++ Assets/Scripts/Weapons/ExplosionVFX.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ExplosionVFX : MonoBehaviour
+    {
+        public float duration;
+        private SpriteRenderer _renderer;
+        private float _elapsed;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void SetSize(float radius)
+        {
+            float size = radius * 2;
+            transform.localScale = new Vector3(size, size, size);
+        }
+
+        private void Update()
+        {
+            _elapsed += Time.deltaTime;
+            if (_elapsed > duration) Destroy(gameObject);
+            Color color = _renderer.color;
+            color.a = 1-_elapsed / duration;
+            _renderer.color = color;
+        }
+    }
+}
