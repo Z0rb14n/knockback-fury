dbfe8a3f290a94b608046f24467a49d77f2ee2fc
JustintyC <justin.ty.cheung@gmail.com>
2023-06-27 16:44:54 -0700

    Player Damage behaviour
    - player collision with enemies is disabled when hit for iframe duration
    - player sprite flashes during iframe duration
    - fixes player flying indefinitely when frog lands on player

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -7,11 +7,13 @@ namespace Player
     public class PlayerHealth : EntityHealth
     {
         private PlayerMovementScript _playerMovement;
+        private SpriteRenderer _playerSprite;
 
         protected override void Awake()
         {
             base.Awake();
             _playerMovement = GetComponent<PlayerMovementScript>();
+            _playerSprite = GetComponent<SpriteRenderer>();
         }
 
         /// <summary>
@@ -22,6 +24,8 @@ namespace Player
             base.DoTakeDamage(dmg);
             _playerMovement.StopMovement();
             StartCoroutine(AllowMovementAfterDelay());
+            StartCoroutine(DisableCollision());
+            
         }
 
         protected override void Die()
@@ -34,6 +38,19 @@ namespace Player
         {
             yield return new WaitForSeconds(iFrameLength * 0.75f);
             _playerMovement.AllowMovement();
+        }
+
+        private IEnumerator DisableCollision()
+        {
+            Physics2D.IgnoreLayerCollision(6, 7, true);
+            for (float i = 0; i < iFrameLength; i += 0.2f)
+            {
+                _playerSprite.color = new Color(1, 1, 1, 0.5f);
+                yield return new WaitForSeconds(0.1f);
+                _playerSprite.color = Color.white;
+                yield return new WaitForSeconds(0.1f);
+            }
+            Physics2D.IgnoreLayerCollision(6, 7, false);
         }
     }
 }
