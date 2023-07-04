0b74a937d648a0bac28a09599201c2b56b30169f
Jensen <liubusiness42@gmail.com>
2023-07-04 16:59:51 -0700

    Completed ranged enemy firing mechanic

---
+++ Assets/Scripts/Enemies/Ranged Enemy/BulletREnemyScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletREnemyScript : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    private float timer;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // TODO - get player's health and deplete it, trigger knockback and iframes
+            Destroy(gameObject);
+        }
+    }
+}
