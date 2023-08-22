ce70400dcba88c6d2d3e9176b97b1ad39d37bb72
Z0rb14n <48940672+Z0rb14n@users.noreply.github.com>
2023-08-21 21:16:43 -0700

    Updated frog Animations, HandMadeScene fix (#40)
    * Swap to new frog; fix thing
    
    * Add Jump and Idle animations
    
    * Fix spawning with rifle in hand made scene

--- Assets/Scripts/Enemies/JumperMovement.cs
+++ Assets/Scripts/Enemies/JumperMovement.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 namespace Enemies
 {
+    [RequireComponent(typeof(Animator))]
     public class JumperMovement : PatrolMovement
     {
-
+        [Tooltip("Jump force (x100)"), Min(0)]
         public float jumpForce;
- 
-
+        
         private bool _canJump;
         private bool _isTouchingSurface;
         private ContactFilter2D _groundFilter;
@@ -17,12 +17,19 @@ namespace Enemies
         private int _physicsCheckMask;
         private bool _canResetVelocity;
         private bool _isStunned;
-        
+        private bool _wasOnWall;
+        private Animator _animator;
+        private static readonly int AnimationJumpHash = Animator.StringToHash("Jump");
+        private static readonly int AnimationWallHash = Animator.StringToHash("IsOnWall");
 
         private bool Grounded => _body.IsTouching(_groundFilter);
         private bool IsOnLeftWall => _body.IsTouching(_leftWallFilter);
         private bool IsOnRightWall => _body.IsTouching(_rightWallFilter);
 
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
 
         private void Start()
         {
@@ -70,7 +77,7 @@ namespace Enemies
             // jump if jumping cooldown is over and entity is in contact with a surface
             if (_canJump && _isTouchingSurface)
             {
-                jump();
+                Jump();
             }
         }
 
@@ -78,18 +85,31 @@ namespace Enemies
         {
             if ((IsOnLeftWall || IsOnRightWall) && !_isStunned)
             {
+                if (!_wasOnWall)
+                {
+                    _wasOnWall = true;
+                    _animator.SetBool(AnimationWallHash, true);
+                }
                 _body.gravityScale = 0;
                 // safeguard against accidental calls to reset velocity after jumping off walls
                 if (_canResetVelocity) _body.velocity = new Vector2(0, 0);
                 _canResetVelocity = false;
-            } 
-            else _body.gravityScale = 1;
+            }
+            else
+            {
+                if (_wasOnWall)
+                {
+                    _wasOnWall = false;
+                    _animator.SetBool(AnimationWallHash, false);
+                }
+                _body.gravityScale = 1;
+            }
         }
 
         // Jumping: if on ground, simply add force
         //          if touching wall, wall jump
         // TODO: jump directions/targeting
-        private void jump()
+        private void Jump()
         {
             if (Grounded)
             {
@@ -103,6 +123,8 @@ namespace Enemies
             {
                 _body.AddForce(new Vector2(-100f, jumpForce * 100));
             }
+
+            _animator.SetTrigger(AnimationJumpHash);
             _canResetVelocity = true;
             StartCoroutine(JumpCooldown());
         }
