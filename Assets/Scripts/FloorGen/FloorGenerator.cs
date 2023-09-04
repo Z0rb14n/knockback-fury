6e485ec520beb80938ffc5ec79721ccdbfdc4b93
Z0rb14n <babybluewoof@gmail.com>
2023-09-03 18:56:42 -0700

    Burst fire kb; raycast hit bomb; NPE; Player has mouser on TestROomGen

--- Assets/Scripts/FloorGen/FloorGenerator.cs
+++ Assets/Scripts/FloorGen/FloorGenerator.cs
@@ -222,7 +222,7 @@ namespace FloorGen
             GameObject weaponPickupObject = Instantiate(weaponPickupPrefab, pos, Quaternion.identity, parent.transform);
             WeaponPickup weaponPickup = weaponPickupObject.GetComponent<WeaponPickup>();
             HashSet<string> playerCurrInventory =
-                PlayerWeaponControl.Instance.GetInventory.Select(data => data.weaponName).ToHashSet();
+                PlayerWeaponControl.Instance.GetInventory.Where(data => data).Select(data => data.weaponName).ToHashSet();
             List<WeaponData> eligibleWeapons =
                 weaponsList.Where(weapon => !playerCurrInventory.Contains(weapon.weaponName)).ToList();
             weaponPickup.weaponData = Instantiate(eligibleWeapons.GetRandom(random));
