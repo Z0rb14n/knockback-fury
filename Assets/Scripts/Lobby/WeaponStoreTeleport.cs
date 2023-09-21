ba5196490c3f83bb4dfa55af8323ef238f6fe41d
Z0rb14n <babybluewoof@gmail.com>
2023-09-20 17:17:24 -0700

    Cover entrance with a wall lol

--- Assets/Scripts/Lobby/WeaponStoreTeleport.cs
+++ Assets/Scripts/Lobby/WeaponStoreTeleport.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Player;
+﻿using Player;
 using UnityEngine;
 using Util;
 
@@ -8,14 +7,32 @@ namespace Lobby
     public class WeaponStoreTeleport : TriggerTextScript
     {
         [SerializeField] private Vector3 positionToTeleport;
+        [SerializeField] private bool isTeleportEnabled = true;
+        [SerializeField] private GameObject objectToDisable;
+
+        private void Awake()
+        {
+            if (objectToDisable)
+            {
+                objectToDisable.SetActive(!isTeleportEnabled);
+                notification.enabled = isTeleportEnabled;
+            }
+        }
+
         protected override void OnPlayerInteraction()
         {
-            PlayerMovementScript.Instance.transform.position = positionToTeleport;
+            if (isTeleportEnabled) PlayerMovementScript.Instance.transform.position = positionToTeleport;
         }
 
         private void OnDrawGizmosSelected()
         {
             Gizmos.DrawWireSphere(positionToTeleport,0.5f);
+        }
+
+        private void OnValidate()
+        {
+            if (objectToDisable) objectToDisable.SetActive(!isTeleportEnabled);
+            notification.enabled = isTeleportEnabled;
         }
     }
 }
