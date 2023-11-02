00af49fa8cb838193b41a8d724d843b08d15fac9
JustintyC <justin.ty.cheung@gmail.com>
2023-11-01 17:55:45 -0700

    HasObstacle() & HasSpaceToMoveUp()

--- Assets/Scripts/Enemies/PatrolMovement.cs
+++ Assets/Scripts/Enemies/PatrolMovement.cs
@@ -10,6 +10,7 @@ namespace Enemies
         public Transform[] patrolPoints;
         public float speed;
         public float pauseTime;
+        public float maxFallHeight;
 
         protected int _target;
         protected Vector2 _targetPos;
@@ -22,6 +23,8 @@ namespace Enemies
         protected Collider2D _collider2D;
 
         private float _originalSpeed;
+        private Vector2 _position;
+        private Vector2 _colliderSize;
         
 
         
@@ -39,6 +42,7 @@ namespace Enemies
             _body = GetComponent<Rigidbody2D>();
             _sprite = GetComponent<SpriteRenderer>();
             _collider2D = GetComponent<Collider2D>();
+            _colliderSize = _collider2D.bounds.size;
             if (patrolPoints.Length > 0)
             {
                 _target = 0;
@@ -59,10 +63,11 @@ namespace Enemies
         }
 
         /// <summary>
-        /// Determine movement direction, change targets if needed
+        /// Determine movement direction, change targets if needed, finds position
         /// </summary>
         protected void DoCommonUpdates()
         {
+            _position = _collider2D.bounds.center;
             DetermineDirection();
             if (Vector2.Distance(transform.position, _targetPos) < _switchTargetDistance)
             {
@@ -77,6 +82,71 @@ namespace Enemies
             {
                 transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
             }
+        }
+
+        /// <summary>
+        /// check for any obstacles ahead at foot level
+        /// </summary>
+        /// <returns> if there is an obstacle </returns>
+        private bool HasObstacle()
+        {
+            Vector2 frontFootPos;
+            Vector2 rayDirection;
+            if (_direction == 1)
+            {
+                frontFootPos = _position + new Vector2(_colliderSize.x / 2, -_colliderSize.y / 2);
+                rayDirection = Vector2.right;
+            }
+            else
+            {
+                frontFootPos = _position - new Vector2(_colliderSize.x / 2, _colliderSize.y / 2);
+                rayDirection = Vector2.left;
+            }
+            int layerMask = ~(1 << LayerMask.NameToLayer("Player"));
+
+
+            return Physics2D.Raycast(frontFootPos, rayDirection, 1, layerMask);
+        }
+
+        /// <summary>
+        /// check if there is space for enemy to move up stairs-like obstacles
+        /// REQUIRES: there is an obstacle ahead
+        /// </summary>
+        /// <returns> height of obstacle that can be moved up, -1 if no space </returns>
+        private float HasSpaceToMoveUp()
+        {
+            float checkAheadDist = 1;
+            Vector2 rayDirection;
+            Vector2 frontTopCornerPos;
+            Vector2 verticalCheckOrigin;
+            float colliderHeight = _colliderSize.y;
+            int layerMask = ~(1 << LayerMask.NameToLayer("Enemy")
+                | 1 << LayerMask.NameToLayer("Player"));
+            if (_direction == 1)
+            {
+                frontTopCornerPos = _position + new Vector2(_colliderSize.x / 2, colliderHeight / 2);
+                rayDirection = Vector2.right;
+                verticalCheckOrigin = frontTopCornerPos + new Vector2(1, 0);
+            }
+            else
+            {
+                frontTopCornerPos = _position - new Vector2(_colliderSize.x / 2, -colliderHeight / 2);
+                rayDirection = Vector2.left;
+                verticalCheckOrigin = frontTopCornerPos - new Vector2(1, 0);
+            }
+            // initial horizontal ray; tests if there are obstacles at head (top corner) level ahead
+            if (Physics2D.Raycast(frontTopCornerPos, rayDirection, checkAheadDist, layerMask)) return -1.0f;
+            
+            // vertical rays up and down from a little bit ahead of head level
+            RaycastHit2D hitDown = Physics2D.Raycast(verticalCheckOrigin, Vector2.down, colliderHeight, layerMask);
+            float distanceToObstacle = hitDown.distance;
+            if (distanceToObstacle + maxFallHeight > colliderHeight) return -1.0f;
+            
+            float rayUpDistance = colliderHeight - distanceToObstacle + 0.1f;
+            RaycastHit2D hitUp = Physics2D.Raycast(verticalCheckOrigin, Vector2.up, rayUpDistance, layerMask);
+
+            if (hitUp) return -1.0f;
+            else return colliderHeight - distanceToObstacle;
         }
 
         // checks if sprite needs flipping; if intended movement direction and sprite direction don't match,
