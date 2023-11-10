47f213cf9d4fe34a32610005aab1739c50d59367
Z0rb14n <babybluewoof@gmail.com>
2023-11-09 23:53:29 -0800

    HAHA HE JUMPS NOW

--- Assets/Scripts/Enemies/Cat/CatBoss.cs
+++ Assets/Scripts/Enemies/Cat/CatBoss.cs
@@ -1,22 +1,40 @@
 ﻿using System;
+using System.Collections;
+using Player;
 using UnityEngine;
 
 namespace Enemies.Cat
 {
-    [RequireComponent(typeof(EntityHealth), typeof(Collider2D), typeof(Rigidbody2D))]
+    [RequireComponent(typeof(EntityHealth), typeof(SpriteRenderer), typeof(Rigidbody2D))]
     public class CatBoss : MonoBehaviour
     {
         [NonSerialized]
         public CatBossManager CatManager;
 
-        [SerializeField] private Vector2 initialVelocity = new Vector2(0, -10);
+        [SerializeField] private Vector2 initialVelocity = new(0, -10);
+        [SerializeField, Min(0)] private float delayBeforeAttacking = 1.5f;
+        [SerializeField, Min(0)] private float delayBetweenJumps = 2.5f;
+        [SerializeField, Min(0)] private Vector2 jumpVector = new(10,10);
+
+        [SerializeField] private Sprite normalSprite;
+        [SerializeField] private Sprite leftSprite;
+        [SerializeField] private Sprite rightSprite;
+        [SerializeField] private Sprite backSprite;
+        [SerializeField] private Collider2D roomCeiling;
         private Collider2D _collider;
         private Rigidbody2D _rigidbody;
+        private LayerMask _groundLayer;
+        private SpriteRenderer _spriteRenderer;
         private BossEnemy _originalBoss;
+        private bool _attacking;
+
+        private bool Grounded => _collider.IsTouchingLayers(_groundLayer);
         private void Awake()
         {
             _collider = GetComponent<Collider2D>();
             _rigidbody = GetComponent<Rigidbody2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _groundLayer = LayerMask.GetMask("Default");
         }
 
         public void PrepareToSpawn(BossEnemy originalBoss)
@@ -24,12 +42,22 @@ namespace Enemies.Cat
             _rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
             _originalBoss = originalBoss;
             _rigidbody.position = new Vector2(originalBoss.transform.position.x, _rigidbody.position.y);
+            Physics2D.IgnoreCollision(_collider, roomCeiling);
         }
 
         public void StartSpawn()
         {
             _rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
             _rigidbody.AddForce(initialVelocity * _rigidbody.mass, ForceMode2D.Impulse);
+            StartCoroutine(DelayBeforeAttacking());
+        }
+
+        private IEnumerator DelayBeforeAttacking()
+        {
+            yield return new WaitForSeconds(delayBeforeAttacking);
+            _attacking = true;
+            Physics2D.IgnoreCollision(_collider, roomCeiling, false);
+            StartCoroutine(JumpCoroutine());
         }
 
         private void DestroyOldBossIfPresent()
@@ -42,9 +70,38 @@ namespace Enemies.Cat
             }
         }
 
+        private void LookAtPlayer()
+        {
+            if (Grounded)
+            {
+                Vector3 playerPos = PlayerMovementScript.Instance.transform.position;
+                Vector2 pos = _rigidbody.position;
+                if (playerPos.x < pos.x) _spriteRenderer.sprite = leftSprite;
+                else if (playerPos.x > pos.x) _spriteRenderer.sprite = rightSprite;
+            }
+        }
+
+        private IEnumerator JumpCoroutine()
+        {
+            while (_attacking)
+            {
+                yield return new WaitForSeconds(delayBetweenJumps);
+                
+                Vector3 playerPos = PlayerMovementScript.Instance.transform.position;
+                Vector2 pos = _rigidbody.position;
+                Vector2 force = jumpVector;
+                if (playerPos.x < pos.x) force *= new Vector2(-1,1);
+                _rigidbody.AddForce(force * _rigidbody.mass, ForceMode2D.Impulse);
+            }
+        }
+
         private void FixedUpdate()
         {
             DestroyOldBossIfPresent();
+            if (_attacking)
+            {
+                LookAtPlayer();
+            }
         }
     }
 }
--- Assets/Scripts/Enemies/Cat/CatBossManager.cs
+++ Assets/Scripts/Enemies/Cat/CatBossManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using Player;
 using UnityEngine;
 
@@ -15,11 +14,9 @@ namespace Enemies.Cat
         [SerializeField] private CatBoss catBoss;
 
         [SerializeField, Min(0)] private float delayBeforeSpawn = 5;
-        [SerializeField] private Collider2D roomCeiling;
 
         private int _triggersRemaining;
         private Collider2D _originalBossCollider;
-        private Collider2D _catCollider;
         private EntityHealth _catHealth;
         private BossHealthBar _bossHealthBar;
 
@@ -30,7 +27,6 @@ namespace Enemies.Cat
             _triggersRemaining = secretTriggers.Length;
             _originalBossCollider = originalBossEnemy.GetComponent<Collider2D>();
             _catHealth = catBoss.GetComponent<EntityHealth>();
-            _catCollider = catBoss.GetComponent<Collider2D>();
             foreach (EntityHealth health in secretTriggers)
             {
                 health.OnDeath += OnDeath;
@@ -50,7 +46,6 @@ namespace Enemies.Cat
         {
             _originalBossCollider.enabled = false;
             catBoss.PrepareToSpawn(originalBossEnemy);
-            Physics2D.IgnoreCollision(_catCollider, roomCeiling);
             CameraScript.Instance.CameraShakeStrength = 1;
             originalBossEnemy.StopBeingActive();
             yield return new WaitForSeconds(delayBeforeSpawn);
