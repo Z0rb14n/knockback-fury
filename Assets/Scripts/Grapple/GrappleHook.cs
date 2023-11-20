a0000edc07bfa5a33fe9c2f11a9c217c6a4f13bd
Jonathan Nah <48940672+Z0rb14n@users.noreply.github.com>
2023-11-19 18:21:24 -0800

    Grapple Hook + Customizable KB + misc changes (#81)
    * Double KB when grounded or on a wall?
    
    * More performant platform impl, bonus KB now modifiable
    
    * Change surface arc angle for platform
    
    * Tbh terraria grapple hook >>> this one
    
    * Disable grapple hook targets; buoyancy in lava
    
    * bro can you not
    
    * Things that ignore platforms now properly ignore them
    
    * more spawn
    
    * Allow using different joints for hook
    
    * more enemies
    
    * Fix lobby platforms unpacked
    
    * Various cool new things
    
    ---------
    
    Co-authored-by: Chiaki9000 <kevinlihaoxuan@gmail.com>

---
+++ Assets/Scripts/Grapple/GrappleHook.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using Player;
+using UnityEngine;
+
+namespace Grapple
+{
+    [RequireComponent(typeof(LineRenderer),typeof(SpringJoint2D), typeof(DistanceJoint2D))]
+    public class GrappleHook : MonoBehaviour
+    {
+        [SerializeField, Min(0)]
+        private float maxDuration = 6;
+        [SerializeField, Min(0)]
+        private float maxLength = 10;
+        [SerializeField]
+        private bool useFixedDistance;
+        [SerializeField, Min(0)]
+        private float fixedDistance;
+        [SerializeField] private bool useDistJoint;
+        private SpringJoint2D _joint;
+        private DistanceJoint2D _distJoint;
+        private Rigidbody2D _body;
+        private Rigidbody2D _playerBody;
+        private PlayerMovementScript _player;
+        private LineRenderer _line;
+        private Collider2D _collider;
+
+        private EntityHealth _hookedEntity;
+
+        private bool _isFixed;
+
+        private void Awake()
+        {
+            _joint = GetComponent<SpringJoint2D>();
+            _distJoint = GetComponent<DistanceJoint2D>();
+            _line = GetComponent<LineRenderer>();
+            _body = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<Collider2D>();
+            _player = PlayerMovementScript.Instance;
+            _playerBody = _player.GetComponent<Rigidbody2D>();
+            _joint.enabled = false;
+            _line.enabled = false;
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_isFixed)
+            {
+                if (Vector2.Distance(_body.position, _playerBody.position) > maxLength)
+                {
+                    _player.OnGrappleOOB();
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                _line.SetPosition(0, _player.transform.position);
+                if (_hookedEntity)
+                {
+                    // TODO optimize
+                    Vector3 hookedPos = _hookedEntity.transform.position;
+                    _line.SetPosition(1, hookedPos);
+                    transform.position = hookedPos;
+                }
+                else
+                {
+                    if (!ReferenceEquals(_hookedEntity, null))
+                    {
+                        _player.OnGrappleExpire();
+                        Destroy(gameObject);
+                    }
+                }
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!_isFixed)
+            {
+                Vector2 pos = other.GetContact(0).point;
+                Vector2 playerPos = _player.transform.position;
+                _isFixed = true;
+                EntityHealth otherEntity = other.collider.GetComponent<EntityHealth>();
+                if (otherEntity && otherEntity.grappleHookTarget)
+                {
+                    pos = otherEntity.transform.position;
+                    _hookedEntity = otherEntity;
+                }
+
+                float dist = Vector2.Distance(pos, playerPos);
+                if (dist > maxLength)
+                {
+                    _player.OnGrappleOOB();
+                    Destroy(gameObject);
+                    return;
+                }
+                
+                _line.SetPosition(0, playerPos);
+                _line.SetPosition(1, pos);
+                _line.enabled = true;
+                _body.constraints = RigidbodyConstraints2D.FreezeAll;
+                Joint2D usedJoint = useDistJoint ? _distJoint : _joint;
+                usedJoint.enabled = true;
+                usedJoint.connectedBody = _playerBody;
+                if (useDistJoint)
+                    _distJoint.distance = useFixedDistance ? fixedDistance : dist;
+                else
+                    _joint.distance = useFixedDistance ? fixedDistance : dist;
+                _collider.enabled = false;
+                StartCoroutine(DestroyAfterDelay());
+            }
+        }
+
+        private IEnumerator DestroyAfterDelay()
+        {
+            yield return new WaitForSeconds(maxDuration);
+            _player.OnGrappleExpire();
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovementScript.cs
+++ Assets/Scripts/Player/PlayerMovementScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using CustomTiles;
 using DashVFX;
 using GameEnd;
+using Grapple;
 using UnityEngine;
 using Upgrades;
 using Weapons;
@@ -37,10 +38,6 @@ namespace Player
         public float accel = 50;
         [Min(0), Tooltip("Acceleration multiplier when turning around")]
         public float turnAroundMultiplier = 2f;
-        [Min(0), Tooltip("Deceleration (scaled by time) when grounded")]
-        public float decel = 20;
-        [Min(0), Tooltip("Deceleration (scaled by time) when airborne")]
-        public float decelAirborne = 5;
         [Min(0), Tooltip("Deceleration (scaled by time) when above max speed")]
         public float decelWhenAbove = 5;
         [Min(0), Tooltip("The height of the short jump compared to the high jump")]
@@ -51,8 +48,18 @@ namespace Player
         public float lateJumpLeeway = 3;
         [Min(0), Tooltip("How long jump needs to be held to jump higher")]
         public float highJumpTime = 3;
-        [Min(0), Tooltip("Time on a wall before walljump is enabled")]
+        [Min(0), Tooltip("Time on a wall before wall jump is enabled")]
         public float minTimeBeforeWallJump = 0.15f;
+        [Min(0), Tooltip("Grounded KB Multiplier for weapons")]
+        public float groundedKBMultiplier = 2;
+        [Min(0), Tooltip("Wall Slid KB Multiplier for weapons")]
+        public float wallKBMultiplier = 2;
+
+        [Header("Grapple Hook")]
+        [Min(0), Tooltip("Grapple Hook Velocity")]
+        public float grappleVelocity = 10;
+        [Tooltip("Grappling hook prefab")]
+        public GameObject grapplePrefab;
 
         private float ActualDashTime => dashTime * (1 + _upgradeManager[UpgradeType.FarStride]);
 
@@ -62,11 +69,11 @@ namespace Player
         {
             get
             {
-                if (_instance == null) _instance = FindObjectOfType<PlayerMovementScript>(true);
-                return _instance;
+                if (instance == null) instance = FindObjectOfType<PlayerMovementScript>(true);
+                return instance;
             }
         }
-        private static PlayerMovementScript _instance;
+        private static PlayerMovementScript instance;
 
         public bool IsWallSliding { get; private set; }
         public bool CanMove { get; set; } = true;
@@ -98,6 +105,7 @@ namespace Player
         private float _jumpTime;
         private bool _isHoldingJump;
         private float _timeOnWall;
+        private GrappleHook _activeGrappleHook;
 
         private bool Grounded => _body.IsTouching(_groundFilter);
         private bool IsOnLeftWall => _body.IsTouching(_leftWallFilter);
@@ -107,7 +115,7 @@ namespace Player
 
         private void Awake()
         {
-            _instance = this;
+            instance = this;
             _body = GetComponent<Rigidbody2D>();
             _meshTrail = GetComponent<MeshTrail>();
             _weapon = GetComponentInChildren<Weapon>();
@@ -170,11 +178,6 @@ namespace Player
                 {
                     newX += xInput * Mathf.Min(maxSpeed - Mathf.Abs(originalX), normalAccel);
                 }
-            }
-            else
-            {
-                float normalDecel = (Grounded ? decel : decelAirborne) * Time.deltaTime;
-                newX -= Mathf.Sign(originalX) * Mathf.Min(Mathf.Abs(originalX), normalDecel);
             }
 
             if (Mathf.Abs(originalX) > maxSpeed)
@@ -240,6 +243,29 @@ namespace Player
             }
         }
 
+        private void GrappleHookLogic()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (_activeGrappleHook) Destroy(_activeGrappleHook.gameObject);
+                GameObject go = Instantiate(grapplePrefab, _body.position, Quaternion.identity);
+                // TODO OPTIMIZE
+                Vector2 worldMousePos = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
+                go.GetComponent<Rigidbody2D>().velocity = ((Vector2)transform.InverseTransformPoint(worldMousePos)).normalized*grappleVelocity + _body.velocity;
+                _activeGrappleHook = go.GetComponent<GrappleHook>();
+            }
+        }
+
+        public void OnGrappleOOB()
+        {
+            _activeGrappleHook = null;
+        }
+
+        public void OnGrappleExpire()
+        {
+            _activeGrappleHook = null;
+        }
+
         private void Update()
         {
             if (Grounded)
@@ -259,9 +285,14 @@ namespace Player
             }
 
             HorizontalMovementLogic(xInput);
+            GrappleHookLogic();
 
             if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)
-                || Input.GetKeyDown(KeyCode.UpArrow)) _jumpRequest = true;
+                                                || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                _jumpRequest = true;
+                if (_activeGrappleHook) Destroy(_activeGrappleHook.gameObject);
+            }
 
             if (Input.GetKeyDown(KeyCode.LeftShift))
             {
@@ -280,6 +311,7 @@ namespace Player
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             {
                 foreach (PlatformTileScript platform in _platformsOn) platform.TemporarilyIgnore();
+                _platformsOn.Clear();
             }
 
             JumpLogic();
@@ -338,6 +370,11 @@ namespace Player
 
         public void RequestKnockback(Vector2 vec, bool isWeapon = false)
         {
+            if (isWeapon)
+            {
+                if (Grounded) vec *= groundedKBMultiplier;
+                else if (IsWallSliding) vec *= wallKBMultiplier;
+            }
             // honestly shouldn't really matter if it's here or just an addForce call
             // but this *feels* slower/unclean but idk
             _knockbackRequest = true;
