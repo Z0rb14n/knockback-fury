cb3a689f26f8aba00f2f3692698a011e8afee6bc
Z0rb14n <babybluewoof@gmail.com>
2023-12-30 19:48:41 -0800

    Grappling Hook Cooldown

--- Assets/Scripts/Upgrades/UpgradeDashDamageCanvas.cs
+++ Assets/Scripts/Upgrades/UpgradeDashDamageCanvas.cs
@@ -1,4 +1,6 @@
-﻿using Player;
+﻿using FileSave;
+using PermUpgrade;
+using Player;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,11 +13,15 @@ namespace Upgrades
         [SerializeField] private RectTransform adrenaline;
         [SerializeField] private RectTransform stabilizedAim;
         [SerializeField] private RectTransform dashCharges;
+        [SerializeField] private Image grappleCooldownObject;
+        [SerializeField] private Image grappleCooldownGrey;
         [SerializeField] private Image targetAnalysisObject;
         [SerializeField] private Image targetAnalysisGrey;
         [SerializeField] private Color targetAnalysisUnavailableColor = new(1, 1, 1, 0.5f);
+        [SerializeField] private Color grappleUnavailableColor = new(1, 1, 1, 0.5f);
         
         private RectTransform _targetAnalysisBackRect;
+        private RectTransform _grappleCooldownBackRect;
         private TextMeshProUGUI _adrenalineText;
         private TextMeshProUGUI _stabilizedAimText;
 
@@ -24,19 +30,22 @@ namespace Upgrades
             _adrenalineText = adrenaline.GetComponentInChildren<TextMeshProUGUI>();
             _stabilizedAimText = stabilizedAim.GetComponentInChildren<TextMeshProUGUI>();
             _targetAnalysisBackRect = targetAnalysisGrey.GetComponent<RectTransform>();
+            _grappleCooldownBackRect = grappleCooldownGrey.GetComponent<RectTransform>();
         }
 
         public void Update()
         {
-            int adrenalineStacks = Mathf.Min(PlayerWeaponControl.Instance.AdrenalineStacks, PlayerWeaponControl.Instance.maxAdrenalineStacks);
+            PlayerMovementScript playerMove = PlayerMovementScript.Instance;
+            PlayerWeaponControl playerWep = PlayerWeaponControl.Instance;
+            int adrenalineStacks = Mathf.Min(playerWep.AdrenalineStacks, playerWep.maxAdrenalineStacks);
             adrenaline.gameObject.SetActive(adrenalineStacks > 0);
             _adrenalineText.text = adrenalineStacks > 1 ? adrenalineStacks.ToString() : "";
             
-            int stabilizedAimStacks = Mathf.Min(PlayerWeaponControl.Instance.StabilizedAimStacks, PlayerWeaponControl.Instance.maxStabilizedAimStacks);
+            int stabilizedAimStacks = Mathf.Min(playerWep.StabilizedAimStacks, playerWep.maxStabilizedAimStacks);
             stabilizedAim.gameObject.SetActive(stabilizedAimStacks > 0);
             _stabilizedAimText.text = stabilizedAimStacks > 1 ? stabilizedAimStacks.ToString() : "";
 
-            int charges = PlayerMovementScript.Instance.EffectiveDashes;
+            int charges = playerMove.EffectiveDashes;
             for (int i = 0; i < dashCharges.childCount; i++)
             {
                 dashCharges.GetChild(i).gameObject.SetActive(i < charges);
@@ -53,6 +62,16 @@ namespace Upgrades
                                               PlayerUpgradeManager.Instance.GetData(UpgradeType.TargetAnalysis));
                 targetAnalysisObject.color = isShieldActive ? Color.white : targetAnalysisUnavailableColor;
                 _targetAnalysisBackRect.anchorMin = new Vector2(ratio, 0);
+            }
+
+            bool hasGrappleHook = CrossRunInfo.HasUpgrade(PermUpgradeType.GrapplingHook);
+            grappleCooldownObject.gameObject.SetActive(hasGrappleHook);
+            if (hasGrappleHook)
+            {
+                bool hasNoCooldown = playerMove.GrappleHookCooldown <= 0;
+                float ratio = hasNoCooldown ? 1 : 1-(playerMove.GrappleHookCooldown / playerMove.grappleCooldown);
+                grappleCooldownObject.color = hasNoCooldown ? Color.white : grappleUnavailableColor;
+                _grappleCooldownBackRect.anchorMin = new Vector2(ratio, 0);
             }
         }
     }
