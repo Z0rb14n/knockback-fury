9f68b6b0cf33ebd44a0134c75c12e9a6cc586873
Z0rb14n <babybluewoof@gmail.com>
2024-04-08 21:52:32 -0700

    Implement Computer Tunnel UI

--- Assets/Scripts/NewFloorGen/ComputerTunnelUI.cs
+++ Assets/Scripts/NewFloorGen/ComputerTunnelUI.cs
@@ -11,9 +11,12 @@ namespace NewFloorGen
         public Transform neighbors;
         [Min(0)]
         public float neighborRadius = 300;
+
+        public ComputerTunnelUIComputer mainComputer;
         private ComputerTunnel _computerTunnel;
         private ComputerRoom _room;
         private int _selectedIndex = -1;
+        private ComputerTunnelUIComputer[] uiNeighbors;
         public void Open(ComputerTunnel trigger)
         {
             gameObject.SetActive(true);
@@ -21,23 +24,22 @@ namespace NewFloorGen
             _computerTunnel.enabled = false;
             _room = _computerTunnel.computerRoom;
             UIUtil.OpenUI();
-            /*
-            for (int i = 0; i < buttonsArea.childCount; i++)
-            {
-                buttonsArea.GetChild(i).gameObject.SetActive(_upgradeTrigger.allowedButtons == null || _upgradeTrigger.allowedButtons.Contains(i));
-            }
-            */
             CreateNeighbors();
+            _selectedIndex = -1;
+            mainComputer.DisplayingVirus = true;
         }
 
         private void CreateNeighbors()
         {
             int len = _room.neighbors.Length;
             ObjectUtil.EnsureLength(neighbors, len, neighborPrefab);
+            uiNeighbors = new ComputerTunnelUIComputer[len];
             for (int i = 0; i < len; i++)
             {
                 ComputerTunnelUIComputer comp = neighbors.GetChild(i).GetComponent<ComputerTunnelUIComputer>();
                 comp.SetRadiusAndAngle(neighborRadius, i * Mathf.PI*2/len);
+                comp.DisplayingVirus = false;
+                uiNeighbors[i] = comp;
             }
         }
 
@@ -45,13 +47,46 @@ namespace NewFloorGen
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                if (_selectedIndex != -1)
+                if (_selectedIndex != -1) Teleport();
+            }
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (_selectedIndex == -1)
+                    ChangeIndex(uiNeighbors.Length-1);
+                else if (_selectedIndex == uiNeighbors.Length - 1)
+                    ChangeIndex(0);
+                else
+                    ChangeIndex(_selectedIndex+1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                switch (_selectedIndex)
                 {
-                    
+                    case -1:
+                        ChangeIndex(0);
+                        break;
+                    case 0:
+                        ChangeIndex(uiNeighbors.Length-1);
+                        break;
+                    default:
+                        ChangeIndex(_selectedIndex-1);
+                        break;
                 }
             }
 
             if (Input.GetKeyDown(KeyCode.Escape)) Close();
+        }
+
+        private void ChangeIndex(int newIndex)
+        {
+            int prevIndex = _selectedIndex;
+            _selectedIndex = newIndex;
+            if (prevIndex == -1) mainComputer.DisplayingVirus = false;
+            else uiNeighbors[prevIndex].DisplayingVirus = false;
+
+            uiNeighbors[_selectedIndex].DisplayingVirus = true;
         }
 
         private void Close()
@@ -64,8 +99,7 @@ namespace NewFloorGen
         public void Teleport()
         {
             Close();
-            // TODO USE CORRECT COMPUTER ROOM
-            _computerTunnel.computerRoom.SpawnPlayer();
+            _room.neighbors[_selectedIndex].SpawnPlayer();
         }
     }
 }
