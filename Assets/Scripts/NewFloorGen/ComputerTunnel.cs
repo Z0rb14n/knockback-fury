83efbf608b2467d44fe102390081f6a3e1087ffc
Z0rb14n <babybluewoof@gmail.com>
2024-04-08 19:43:09 -0700

    aa

---
+++ Assets/Scripts/NewFloorGen/ComputerTunnel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Util;
+
+namespace NewFloorGen
+{
+    public class ComputerTunnel : TriggerTextScript
+    {
+        public ComputerTunnelUI tunnelUI;
+
+        private void Awake()
+        {
+            if (!tunnelUI) tunnelUI = FindObjectOfType<ComputerTunnelUI>();
+        }
+
+        protected override void OnPlayerInteraction()
+        {
+            tunnelUI.Open(this);
+        }
+    }
+}
---
+++ Assets/Scripts/NewFloorGen/ComputerTunnelUI.cs
@@ -0,0 +1,27 @@
+﻿using Player;
+using UnityEngine;
+
+namespace NewFloorGen
+{
+    public class ComputerTunnelUI : MonoBehaviour
+    {
+        public GameObject visibleUI;
+        private ComputerTunnel _computerTunnel;
+        private int _selectedIndex;
+        public void Open(ComputerTunnel trigger)
+        {
+            _computerTunnel = trigger;
+            _computerTunnel.enabled = false;
+            Time.timeScale = 0;
+            PlayerMovementScript.Instance.enabled = false;
+            PlayerWeaponControl.Instance.enabled = false;
+            /*
+            for (int i = 0; i < buttonsArea.childCount; i++)
+            {
+                buttonsArea.GetChild(i).gameObject.SetActive(_upgradeTrigger.allowedButtons == null || _upgradeTrigger.allowedButtons.Contains(i));
+            }
+            */
+            visibleUI.SetActive(true);
+        }
+    }
+}
