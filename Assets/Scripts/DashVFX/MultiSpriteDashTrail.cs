fb922f11ed4e6afb97dd8a0c17e39adcc8dc40f2
Z0rb14n <babybluewoof@gmail.com>
2025-09-27 00:50:16 -0700

    Migrate to using Rigidbody2D.Slide

---
+++ Assets/Scripts/DashVFX/MultiSpriteDashTrail.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace DashVFX
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public abstract class MultiSpriteDashTrail: AbstractDashTrail
+    {
+        [SerializeField] protected GameObject objectToHide;
+        [SerializeField] protected SpriteRenderer[] objects;
+        [SerializeField] protected float velocityScale = 0.1f;
+
+        protected SpriteRenderer spriteRenderer;
+        protected bool vfxEnabled;
+        
+        protected virtual void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        protected void FixedUpdate()
+        {
+            if (!vfxEnabled) return;
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].sprite = spriteRenderer.sprite;
+                objects[i].transform.localPosition = -(i + 1) * velocityScale * GetObjectVelocity();
+            }
+        }
+
+        protected abstract Vector2 GetObjectVelocity();
+
+        public override void StartDash(bool flipped)
+        {
+            vfxEnabled = true;
+            foreach (SpriteRenderer sr in objects) sr.flipX = flipped;
+            objectToHide.SetActive(true);
+        }
+
+        public override void StopDash()
+        {
+            vfxEnabled = false;
+            objectToHide.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/DashVFX/RigidbodyDashTrail.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace DashVFX
+{
+    [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
+    public class RigidbodyDashTrail : MultiSpriteDashTrail
+    {
+        private Rigidbody2D _rigidbody;
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        protected override Vector2 GetObjectVelocity()
+        {
+            return _rigidbody.linearVelocity;
+        }
+    }
+}
