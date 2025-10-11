66b5f1a34a88e69640dc43a003bd4fed405f50b1
Z0rb14n <babybluewoof@gmail.com>
2025-10-10 20:43:32 -0700

    misc cleanup of settings canvas and build settings

---
+++ Assets/Scripts/UI/Options/OptionsMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UI.Options
+{
+    public class OptionsMenu : MonoBehaviour
+    {
+        public void OnOptionsMenuButtonClicked()
+        {
+            
+        }
+        
+        public void OnBackButtonClicked()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
