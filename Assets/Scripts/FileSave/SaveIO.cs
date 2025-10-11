b1b1f9a5b98b4df506a52a0d33ac337005c63e48
Z0rb14n <babybluewoof@gmail.com>
2025-10-10 20:54:51 -0700

    delete extra main menu + fix save IO on path change + fix pointer + add tutorial to build settings

--- Assets/Scripts/FileSave/SaveIO.cs
+++ Assets/Scripts/FileSave/SaveIO.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEngine;
 using Application = UnityEngine.Device.Application;
 
@@ -18,6 +19,16 @@ namespace FileSave
 
         public static SaveData Read()
         {
+            if (!File.Exists(saveLocation))
+            {
+                Debug.Log("No save file found, using defaults.");
+                return new SaveData
+                {
+                    cheese = 0,
+                    unlockedPermanentUpgrades = Array.Empty<int>(),
+                    unlockedWeapons = Array.Empty<string>()
+                };
+            }
             return JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
         }
     }
