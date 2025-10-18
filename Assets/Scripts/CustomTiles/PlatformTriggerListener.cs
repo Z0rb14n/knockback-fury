5405b96b30172b9c6b51411bef39c3fa439c3e09
Z0rb14n <babybluewoof@gmail.com>
2025-10-18 15:27:13 -0700

    platform overlap fix

--- Assets/Scripts/CustomTiles/PlatformTriggerListener.cs
+++ Assets/Scripts/CustomTiles/PlatformTriggerListener.cs
@@ -10,12 +10,17 @@ namespace CustomTiles
         private Collider2D _playerCollider;
         private Collider2D _collider;
 
+        private int _platformLayer;
+        private int _platformIgnoredLayer;
+
         private void Awake()
         {
             _tile = GetComponentInParent<PlatformTileScript>();
             _collider = _tile.GetComponent<Collider2D>();
             _playerMovement = PlayerMovementScript.Instance;
             _playerCollider = _playerMovement.GetComponent<Collider2D>();
+            _platformLayer = LayerMask.NameToLayer("Platform");
+            _platformIgnoredLayer = LayerMask.NameToLayer("PlatformIgnored");
         }
 
         private void OnTriggerStay2D(Collider2D other)
@@ -23,7 +28,8 @@ namespace CustomTiles
             if (other != _playerCollider) return;
             ColliderDistance2D dist = _collider.Distance(_playerCollider);
             float y = dist.normal.y * dist.distance;
-            _tile.gameObject.layer = LayerMask.NameToLayer(y < 0 ? "Platform" : "PlatformIgnored");
+            bool shouldBeEnabled = dist.isOverlapped ? _tile.gameObject.layer == _platformLayer : y < 0;
+            _tile.gameObject.layer = shouldBeEnabled ? _platformLayer : _platformIgnoredLayer;
         }
 
         private void OnTriggerExit2D(Collider2D other)
